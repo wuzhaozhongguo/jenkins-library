36071a17b5706e824935922f9a1915a4abdce93f
Linda Siebert <linda.siebert@sap.com>
2022-03-17 17:33:32 +0100

    Switch type

--- vars/piperExecuteBin.groovy
+++ vars/piperExecuteBin.groovy
@@ -19,7 +19,7 @@ void call(Map parameters = [:], String stepName, String metadataFile, List crede
         handlePipelineStepErrorsParameters.failOnError = true
     }
 
-    Map generalConfig = []
+    List generalConfig = []
 
     handlePipelineStepErrors(handlePipelineStepErrorsParameters) {
         Script script = checkScript(this, parameters) ?: this
