763d968056c18a9b02ed76a13fbd7b05270e3737
Oliver Feldmann <oliver.feldmann@sap.com>
2022-03-29 15:09:19 +0200

    dont fail

--- vars/dockerExecuteOnKubernetes.groovy
+++ vars/dockerExecuteOnKubernetes.groovy
@@ -321,12 +321,12 @@ void executeOnPod(Map config, utils, Closure body, Script script) {
                     echo "ContainerConfig: ${containerParams}"
                     container(containerParams) {
                         try {
-                            sh 'echo 1111111111 && cat $PIPER_ansEventTemplateFilePath'
+                            sh 'echo 1111111111 && cat $PIPER_ansEventTemplateFilePath || true'
                             utils.unstashAll(stashContent)
-                            sh 'echo 2222222222 && cat $PIPER_ansEventTemplateFilePath'
+                            sh 'echo 2222222222 && cat $PIPER_ansEventTemplateFilePath || true'
                             echo "invalidate stash workspace-${config.uniqueId}"
                             stash name: "workspace-${config.uniqueId}", excludes: '**/*', allowEmpty: true
-                            sh 'echo 3333333333 && cat $PIPER_ansEventTemplateFilePath'
+                            sh 'echo 3333333333 && cat $PIPER_ansEventTemplateFilePath || true'
                             body()
                         } finally {
                             stashWorkspace(config, 'container', true, true)
